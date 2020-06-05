58679bbbd84536342c6da000ade8bcdb7c4e7808
虎牙小白菜 <675917107@qq.com>
2020-06-05 09:21:39 +0800

    添加koa-compose方法

--- 26接口轮询方案解决.js
+++ 26接口轮询方案解决.js
@@ -44,3 +44,92 @@ const pollFun = (next) => {
 }
 
 rollPolling(pollFun)
+
+
+
+
+// 第二种方式 koa-compose
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <script>
+    
+    
+    /**
+* pollFun 轮询函数 function
+*/
+const rollPolling = (pollFun, timeInterval) => {
+  pollFun && pollFun()
+  let arrLen = 2
+  let fetchList = []
+  function compose(middleware) {
+      return function () {
+        fetchList.shift()
+        let index = -1
+        return dispatch(0)
+        function dispatch(i) {
+          const startTime = +new Date()
+          if (i <= index) return Promise.reject(new Error('next() called multiple times'))
+          index = i
+          const fn = middleware[i]
+          if (!fn) return Promise.resolve()
+          try {
+            fetchList.shift()
+            return Promise.resolve(fn(function next() {
+              const endTime = +new Date()
+              console.log(endTime - startTime)
+              return dispatch(i + 1)
+            }))
+          } catch (err) {
+            return Promise.reject(fn(function next() {
+              console.log(err, 'err')
+              const endTime = +new Date()
+              console.log(endTime - startTime)
+              return dispatch(i + 1)
+            }))
+          }
+        }
+      }
+    }
+  const reduceArr = (fucArr) => {
+    return fucArr.reduce((pre, cur) => {
+      return (next) => {
+        return pre((n) => {
+          fetchList.shift()
+          return cur(next, n)
+        })
+      }
+    })
+  }
+  const asyncFun = (next) => {
+    pollFun(next)
+  }
+  setInterval(() => {
+    if (fetchList.length === 0) {
+      for (let i = 0; i < arrLen; i++) {
+        fetchList.push(asyncFun)
+      }
+      compose(fetchList)()
+    }
+  }, 2000) // 轮询接口的速度 由于存在每次push进入多个 ajax 所以不会存在定时轮询 只会在前一个接口完成后 再去请求下一个接口
+}
+
+
+// 使用方法
+// 示例轮询函数
+const pollFun = (next) => {
+  setTimeout(() => {
+    next && next()
+  }, 2000)
+}
+
+rollPolling(pollFun, 3000)
+  </script>
+</body>
+</html>

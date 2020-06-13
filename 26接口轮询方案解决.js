1b07984b83bf32501239a8f3ab27622cad0101ee
虎牙小白菜 <675917107@qq.com>
2020-06-13 12:59:25 +0800

    Update 26接口轮询方案解决.js

--- 26接口轮询方案解决.js
+++ 26接口轮询方案解决.js
@@ -61,75 +61,64 @@ rollPolling(pollFun)
   <script>
     
     
-    /**
+
+/**
 * pollFun 轮询函数 function
 */
 const rollPolling = (pollFun, timeInterval) => {
+  // 轮询时 首先执行一次获取数据
   pollFun && pollFun()
-  let arrLen = 2
-  let fetchList = []
-  function compose(middleware) {
-      return function () {
+  let fetchList = [] // 请求的列表
+  function compose() {
+    return function () {
+      let index = -1
+      return dispatch(0)
+      function dispatch(i) {
+        if (i <= index) return Promise.reject(new Error('next() called multiple times'))
+        index = i
+        const fn = fetchList[i]
         fetchList.shift()
-        let index = -1
-        return dispatch(0)
-        function dispatch(i) {
-          const startTime = +new Date()
-          if (i <= index) return Promise.reject(new Error('next() called multiple times'))
-          index = i
-          const fn = middleware[i]
-          if (!fn) return Promise.resolve()
-          try {
-            fetchList.shift()
-            return Promise.resolve(fn(function next() {
-              const endTime = +new Date()
-              console.log(endTime - startTime)
-              return dispatch(i + 1)
-            }))
-          } catch (err) {
-            return Promise.reject(fn(function next() {
-              console.log(err, 'err')
-              const endTime = +new Date()
-              console.log(endTime - startTime)
-              return dispatch(i + 1)
-            }))
-          }
+        if (!fn) return Promise.resolve()
+        try {
+          return Promise.resolve(fn(function next() {
+            return dispatch(i + 1)
+          }))
+        } catch (err) {
+          return Promise.reject(fn(function next() {
+            console.log(err, 'err')
+            // const endTime = +new Date()
+            // console.log(endTime - startTime)
+            return dispatch(i + 1)
+          }))
         }
       }
     }
-  const reduceArr = (fucArr) => {
-    return fucArr.reduce((pre, cur) => {
-      return (next) => {
-        return pre((n) => {
-          fetchList.shift()
-          return cur(next, n)
-        })
-      }
-    })
   }
+  // 构造一个异步函数
   const asyncFun = (next) => {
     pollFun(next)
   }
+  const arrLen = 1
   setInterval(() => {
     if (fetchList.length === 0) {
-      for (let i = 0; i < arrLen; i++) {
+      for (let i = 0; arrLen < 1; i++) {
         fetchList.push(asyncFun)
       }
       compose(fetchList)()
     }
-  }, 2000) // 轮询接口的速度 由于存在每次push进入多个 ajax 所以不会存在定时轮询 只会在前一个接口完成后 再去请求下一个接口
+  }, timeInterval) // 轮询接口的速度 由于存在每次push进入多个 ajax 所以不会存在定时轮询 只会在前一个接口完成后 再去请求下一个接口
 }
-
-
 // 使用方法
 // 示例轮询函数
 const pollFun = (next) => {
+  const time = (Math.random() * 1000).toFixed(2)
+  console.log(time, ' time')
   setTimeout(() => {
     next && next()
-  }, 2000)
+  }, time )
 }
 
-rollPolling(pollFun, 3000)
+rollPolling(pollFun, 6000)
   </script>
 </body>
 </html>
